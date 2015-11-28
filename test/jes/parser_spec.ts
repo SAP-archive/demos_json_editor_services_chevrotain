417e5cb45852fe3fbdad1f0e4b1a74e32940fe2b
Soel <shachar.soel@sap.com>
2015-11-29 00:52:20 +0200

    better sanity test for parser.

--- test/jes/parser_spec.ts
+++ test/jes/parser_spec.ts
@@ -7,7 +7,16 @@ namespace jes.parser.spec {
 
         it("can lex & parse a simple Json - sanity test", () => {
 
-            let inputText = "{ \"arr\": [1,2,3], \"obj\": {\"num\":666}}";
+            let inputText = `{
+                "key1" : 666,
+                "key2" : "bamba",
+                "key3" : true,
+                "key4" : false,
+                "key5" : null,
+                "key6" : [1,2,3],
+                "key7" : { "innerkey" : 444}
+                }`
+
             let lexResult = JsonLexer.tokenize(inputText);
             expect(lexResult.errors).to.be.empty
 
